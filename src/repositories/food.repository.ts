2475c1b1d953c71ea55c56e5d8bcf2d76ea65a32
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-11-15 14:22:24 +0700

    Add find on food repository

--- src/repositories/food.repository.ts
+++ src/repositories/food.repository.ts
@@ -1,10 +1,13 @@
-import { Collection, ObjectId } from 'mongodb'
+import { Collection, Filter, ObjectId } from 'mongodb'
 import { BaseRepo } from '../interfaces/repository.interface'
-import { FoodAttributes, FoodCreationAttributes } from '../models/food.model'
+import { FoodAttributes, FoodCreationAttributes, FoodModel } from '../models/food.model'
 import { DatabaseModels } from '../models'
+import { ListOptions } from '../../proto_gen/common_pb'
+import { getDateFromUnix } from '../utils/time'
+import { stringToBool } from '../utils/value'
 
 export class FoodRepository extends BaseRepo {
-    private foodCollection!: Collection
+    private foodCollection!: Collection<FoodModel>
 
     init(db: DatabaseModels): void {
         this.foodCollection = db.foodCollection
@@ -17,5 +20,112 @@ export class FoodRepository extends BaseRepo {
 
     findById = async (id: string): Promise<FoodAttributes | null> => {
         return this.foodCollection.findOne<FoodAttributes>({ _id: new ObjectId(id) })
+    }
+
+    find = async (options: ListOptions): Promise<FoodAttributes[]> => {
+        // prepare filter
+        const filter: Filter<FoodAttributes> = {}
+        this.prepareFindFilter(filter, options)
+
+        const sort = options.getSortBy()
+
+        const list = await this.foodCollection.find(filter, {
+            limit: options.getLimit(),
+            skip: options.getSkip(),
+            sort: sort ? [[sort.getColumn(), sort.getDirection() == 1 ? -1 : 1]] : undefined,
+        })
+
+        return list.toArray()
+    }
+
+    private prepareFindFilter = (filter: Filter<FoodAttributes>, options: ListOptions) => {
+        const name = options.getFilterMap().get("name")
+
+        const priceGt = options.getFilterMap().get("price.gt")
+        const priceLt = options.getFilterMap().get("price.lt")
+
+        const createdAtGt = options.getFilterMap().get("createdAt.gt")
+        const createdAtGtUnix = createdAtGt ? parseInt(createdAtGt) : undefined;
+        const createdAtGtDate = createdAtGtUnix && !Number.isNaN(createdAtGtUnix) ? getDateFromUnix(createdAtGtUnix) : undefined
+
+        const createdAtLt = options.getFilterMap().get("createdAt.lt")
+        const createdAtLtUnix = createdAtLt ? parseInt(createdAtLt) : undefined;
+        const createdAtLtDate = createdAtLtUnix && !Number.isNaN(createdAtLtUnix) ? getDateFromUnix(createdAtLtUnix) : undefined
+
+        const updatedAtGt = options.getFilterMap().get("updatedAt.gt")
+        const updatedAtGtUnix = updatedAtGt ? parseInt(updatedAtGt) : undefined;
+        const updatedAtGtDate = updatedAtGtUnix && !Number.isNaN(updatedAtGtUnix) ? getDateFromUnix(updatedAtGtUnix) : undefined
+
+        const updatedAtLt = options.getFilterMap().get("updatedAt.lt")
+        const updatedAtLtUnix = updatedAtLt ? parseInt(updatedAtLt) : undefined;
+        const updatedAtLtDate = updatedAtLtUnix && !Number.isNaN(updatedAtLtUnix) ? getDateFromUnix(updatedAtLtUnix) : undefined
+
+        const caloriesGt = options.getFilterMap().get("detail.calories.gt")
+        const caloriesLt = options.getFilterMap().get("detail.calories.lt")
+
+        const sugarGt = options.getFilterMap().get("detail.sugar.gt")
+        const sugarLt = options.getFilterMap().get("detail.sugar.lt")
+
+        const visible = options.getFilterMap().get("visible")
+
+        if (name) {
+            filter.name = {
+                $regex: new RegExp(`.*${name}.*`)
+            }
+        }
+
+        if (priceGt || priceLt) {
+            filter.price = {}
+            if (priceGt) {
+                filter.price.$gte = parseInt(priceGt)
+            }
+            if (priceLt) {
+                filter.price.$lte = parseInt(priceLt)
+            }
+        }
+
+        if (createdAtGtDate || createdAtLtDate) {
+            filter.createdAt = {}
+            if (createdAtGtDate) {
+                filter.createdAt.$gte = createdAtGtDate
+            }
+            if (createdAtLtDate) {
+                filter.createdAt.$lte = createdAtLtDate
+            }
+        }
+
+        if (updatedAtGtDate || updatedAtLtDate) {
+            filter.updatedAt = {}
+            if (updatedAtGtDate) {
+                filter.updatedAt.$gte = updatedAtGtDate
+            }
+            if (updatedAtLtDate) {
+                filter.updatedAt.$lte = updatedAtLtDate
+            }
+        }
+
+        if (caloriesGt || caloriesLt) {
+            filter["detail.calories"] = {}
+            if (caloriesGt) {
+                filter["detail.calories"].$gte = parseInt(caloriesGt)
+            }
+            if (caloriesLt) {
+                filter["detail.calories"].$lte = parseInt(caloriesLt)
+            }
+        }
+
+        if (sugarGt || sugarLt) {
+            filter["detail.sugar"] = {}
+            if (sugarGt) {
+                filter["detail.sugar"].$gte = parseInt(sugarGt)
+            }
+            if (sugarLt) {
+                filter["detail.sugar"].$lte = parseInt(sugarLt)
+            }
+        }
+
+        if (visible) {
+            filter.visible = stringToBool(visible, true)
+        }
     }
 }
--- tests/integration/repositories/food.repository.test.ts
+++ tests/integration/repositories/food.repository.test.ts
@@ -1,7 +1,9 @@
-import { createDatabaseModels } from '../../../src/models'
+import { MongoClient } from 'mongodb'
+import { DatabaseModels, createDatabaseModels } from '../../../src/models'
 import { FoodCreationAttributes } from '../../../src/models/food.model'
 import { AppRepository } from '../../../src/repositories'
 import crypto from 'crypto'
+import { ListOptions, SortBy } from '../../../proto_gen/common_pb'
 
 describe('FoodRepository.insert Test', () => {
     test('Should insert food to the db', async () => {
@@ -26,6 +28,7 @@ describe('FoodRepository.insert Test', () => {
             createdAt: new Date(),
             updatedAt: new Date(),
             version: 1,
+            visible: false,
         }
 
         // execute
@@ -77,6 +80,7 @@ describe("FoodRepository.findOne Test", () => {
             createdAt: new Date(),
             updatedAt: new Date(),
             version: 1,
+            visible: true,
         }
         const { insertedId } = await dbModels.foodCollection.insertOne(foodModel)
         foodModel._id = insertedId
@@ -89,5 +93,373 @@ describe("FoodRepository.findOne Test", () => {
         // closing
         await dbModels.foodCollection.deleteOne({ _id: foodModel._id })
         dbClient.close()
+    })
+})
+
+describe("FoodRepository.find Test", () => {
+    let dbModels: DatabaseModels
+    let dbClient: MongoClient
+
+    beforeAll(async () => {
+        // prepare db
+        [dbModels, dbClient] = createDatabaseModels()
+
+        // prepare data
+        await dbModels.foodCollection.insertMany([{
+            name: 'Food 1',
+            images: [],
+            price: 10000,
+            qty: 10,
+            detail: {
+                calories: 100,
+                sugar: 5,
+                vitamin: {},
+            },
+            createdAt: new Date('2023-10-10'), // 1696896000
+            updatedAt: new Date('2023-11-04'), // 1699056000
+            version: 1,
+            visible: true,
+        }, {
+            name: 'Food 2',
+            images: [],
+            price: 15000,
+            qty: 15,
+            detail: {
+                calories: 250,
+                sugar: 10,
+                vitamin: {},
+            },
+            createdAt: new Date('2023-10-15'), // 1697328000
+            updatedAt: new Date('2023-11-05'), // 1699142400
+            version: 1,
+            visible: true,
+        }, {
+            name: 'Food 3',
+            images: [],
+            price: 25000,
+            qty: 5,
+            detail: {
+                calories: 400,
+                sugar: 12,
+                vitamin: {},
+            },
+            createdAt: new Date('2023-10-25'), // 1698192000
+            updatedAt: new Date('2023-11-03'), // 1698969600
+            version: 1,
+            visible: false,
+        }])
+    })
+
+    afterAll(async () => {
+        await dbModels.foodCollection.drop()
+        await dbClient.close()
+    })
+
+    test("Should return the whole items", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(3)
+    })
+
+    test("Should return limit 2", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setLimit(2)
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return limit 2 and skip 1", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setLimit(2)
+            .setSkip(1)
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return descending", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setSortBy(new SortBy()
+                .setColumn("name").setDirection(SortBy.Direction.DESC))
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result[0].name).toBe("Food 3")
+    })
+
+    test("Should return filtered by name", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        const filter = listOptions.getFilterMap()
+        filter.set("name", "1")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(1)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return filtered range by price", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("price.gt", "15000")
+            .set("price.lt", "30000")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return filtered range by only greater than requested price", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("price.gt", "15000")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return filtered range by createdAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setSortBy(
+                new SortBy().setColumn("createdAt").setDirection(SortBy.Direction.ASC)
+            )
+
+        listOptions.getFilterMap()
+            .set("createdAt.gt", "1696895000")
+            .set("createdAt.lt", "1697329000")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+
+    test("Should return filtered range by only greater than requested createdAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setSortBy(
+                new SortBy().setColumn("createdAt").setDirection(SortBy.Direction.ASC)
+            )
+
+        listOptions.getFilterMap()
+            .set("createdAt.gt", "1697327000")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return not filtered range by createdAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("createdAt.gt", "date")
+            .set("createdAt.lt", "date")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(3)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return filtered range by updatedAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setSortBy(
+                new SortBy().setColumn("updatedAt").setDirection(SortBy.Direction.ASC)
+            )
+
+        listOptions.getFilterMap()
+            .set("updatedAt.gt", "1699055000")
+            .set("updatedAt.lt", "1699143400")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+
+    test("Should return filtered range by only greater than requested updatedAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+            .setSortBy(
+                new SortBy().setColumn("updatedAt").setDirection(SortBy.Direction.ASC)
+            )
+
+        listOptions.getFilterMap()
+            .set("updatedAt.gt", "1699055000")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return not filtered range by updatedAt", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("updatedAt.gt", "date")
+            .set("updatedAt.lt", "date")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(3)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return filtered by calories", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("detail.calories.gt", "200")
+            .set("detail.calories.lt", "500")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return filtered by only greater than requested calories", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("detail.calories.gt", "100")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(3)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return filtered by sugar", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("detail.sugar.gt", "5")
+            .set("detail.sugar.lt", "10")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
+    })
+
+    test("Should return filtered by only greater than requested sugar", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("detail.sugar.gt", "10")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 2")
+    })
+
+    test("Should return filtered visible", async () => {
+        // initialize repo
+        const repo = new AppRepository()
+        repo.init(dbModels)
+
+        // prepare payload
+        const listOptions = new ListOptions()
+        listOptions.getFilterMap()
+            .set("visible", "true")
+
+        // execution
+        const result = await repo.foodRepo.find(listOptions)
+        expect(result.length).toBe(2)
+        expect(result[0].name).toBe("Food 1")
     })
 })
