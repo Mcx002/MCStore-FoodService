2f8a9a71f089c8704bcb1884850e7c7a5e38193d
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-11-15 07:45:55 +0700

    Add get by id on food service

--- src/services/food.service.ts
+++ src/services/food.service.ts
@@ -1,10 +1,11 @@
 import { App } from '../app'
 import { BaseService } from '../interfaces/service.interface'
-import { FoodModel } from '../models/food.model'
+import { FoodAttributes, FoodCreationAttributes } from '../models/food.model'
 import { FoodRepository } from '../repositories/food.repository'
 import { FoodDto } from '../../proto_gen/food_pb'
 import { ErrorHandler } from '../adapter/error.adapter'
 import { Status } from '@grpc/grpc-js/build/src/constants'
+import { getUnixFromDate } from '../utils/time'
 
 export class FoodService extends BaseService {
     foodRepo!: FoodRepository
@@ -30,7 +31,7 @@ export class FoodService extends BaseService {
         }
 
         // create food model
-        const foodModel: FoodModel = {
+        const foodModel: FoodCreationAttributes = {
             name: payload.getName(),
             price: payload.getPrice(),
             qty: payload.getQty(),
@@ -40,24 +41,40 @@ export class FoodService extends BaseService {
                 sugar: payloadDetail.getSugar(),
                 vitamin,
             },
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            version: 1,
         }
 
         // execute insert
         await this.foodRepo.insert(foodModel)
 
         // return
-        return this.composeFoodDto(foodModel)
+        return this.composeFoodDto(foodModel as FoodAttributes)
     }
 
-    composeFoodDto = (data: FoodModel): FoodDto => {
+    getById = async (id: string): Promise<FoodDto> => {
+        // find food by id
+        const data = await this.foodRepo.findById(id)
+        if (!data) {
+            throw new ErrorHandler(Status.INVALID_ARGUMENT, `Food with id: ${id}, not found`)
+        }
+
+        return this.composeFoodDto(data)
+    }
+
+    composeFoodDto = (data: FoodAttributes): FoodDto => {
+        // compose foodDtoDetail
         const foodDtoDetail = new FoodDto.Detail()
             .setCalories(data.detail.calories)
             .setSugar(data.detail.sugar)
 
+        // compose vitamin foodDtoDetail
         for (const key in data.detail.vitamin) {
             foodDtoDetail.getVitaminMap().set(key, data.detail.vitamin[key])
         }
 
+        // compose foodDto
         return new FoodDto()
             .setName(data.name)
             .setImagesList(data.images)
@@ -65,5 +82,8 @@ export class FoodService extends BaseService {
             .setQty(data.qty)
             .setDetail(foodDtoDetail)
             .setId(data._id!.toString())
+            .setCreatedAt(getUnixFromDate(data.createdAt))
+            .setUpdatedAt(getUnixFromDate(data.updatedAt))
+            .setVersion(data.version)
     }
 }
--- tests/unit/services/food.service.test.ts
+++ tests/unit/services/food.service.test.ts
@@ -1,7 +1,8 @@
 import { App } from '../../../src/app'
 import { FoodDto } from '../../../proto_gen/food_pb'
-import { FoodModel } from '../../../src/models/food.model'
+import { FoodAttributes, FoodCreationAttributes } from '../../../src/models/food.model'
 import { ObjectId } from 'mongodb'
+import crypto from 'crypto'
 
 describe('FoodService.create Test', () => {
     test('Should throw error invalid args', () => {
@@ -24,7 +25,7 @@ describe('FoodService.create Test', () => {
         const id = 'B666C267D37FA04BC73A252E'.toLowerCase()
         const _id = new ObjectId(id)
         jest.spyOn(app.repository.foodRepo, 'insert').mockImplementation(
-            async (data: FoodModel) => {
+            async (data: FoodCreationAttributes) => {
                 data._id = _id
             }
         )
@@ -47,6 +48,56 @@ describe('FoodService.create Test', () => {
         const foodResult = await app.service.foodService.create(foodDto)
 
         expect(foodDto.getName()).toBe(foodResult.getName())
+        expect(foodResult.getId()).toBe(id)
+    })
+})
+
+describe('FoodService.getById Test', () => {
+    test("Should throw food not found", () => {
+        // prepare service
+        const app = new App()
+        app.service.init(app)
+
+        // mock repo
+        jest.spyOn(app.repository.foodRepo, 'findById').mockImplementation(
+            async () => {
+                return null
+            }
+        )
+
+        // execute
+        expect(() => app.service.foodService.getById('test')).rejects.toThrow("Food with id: test, not found")
+    })
+    test("Should return food", async () => {
+        // prepare service
+        const app = new App()
+        app.service.init(app)
+
+        // mock repo
+        jest.spyOn(app.repository.foodRepo, 'findById').mockImplementation(
+            async (id) => {
+                return {
+                    _id: new ObjectId(id),
+                    name: 'Food',
+                    price: 13000,
+                    qty: 10,
+                    images: [],
+                    detail: {
+                        calories: 10,
+                        sugar: 1,
+                        vitamin: {},
+                    },
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    version: 1,
+                }
+            }
+        )
+
+        // execute
+        const id = crypto.randomBytes(12).toString('hex')
+        const foodResult = await app.service.foodService.getById(id)
+
         expect(foodResult.getId()).toBe(id)
     })
 })
